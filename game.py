909383ca7ac5453957b8aaca9a72d2544cc8a969
monicamfm <monicamfmiras@gmail.com>
2022-03-15 13:11:07 +0100

    ladrillos en lista

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 from curses import BUTTON1_RELEASED
-from ssl import VERIFY_X509_PARTIAL_CHAIN
-from sys import orig_argv
 import pygame as pg 
-import random
+import random 
 pg.init()
-class Vigneta:
+
+
+FPS = 60
+class Vigneta: #sacar una clase que saque los atributos necesarios que haga dibujos en la pantalla
     def __init__(self, padre, x, y, ancho, alto, color =(255, 255, 255)):
         self.padre = padre
         self.x = x
@@ -15,8 +16,16 @@ class Vigneta:
         self.vx = 0
         self.vy = 0
 
+    @property #decorador: a la funcion que deficines le mete caracteristicas nuevas
+    def xcentro(self):
+        return self.x + self.ancho // 2
+
+    @property
+    def ycentro(self):
+        return self.y + self.alto // 2
+
     def dibujar(self):
-        pass 
+        pass
 
     def mover(self):
         pass
@@ -26,10 +35,10 @@ class Ladrillo(Vigneta):
     def dibujar(self):
         pg.draw.rect(self.padre, self.color, (self.x, self.y, self.ancho, self.alto))
 
-    def comprobarToque(self, bola):
+""" def comprobarToque(self, bola):
         #hago cosas al tocarme la bola
         pass
-
+"""
 
 class Raqueta(Vigneta):
     def __init__(self, padre, x, y, ancho, alto, color = (255, 255, 0)):
@@ -39,7 +48,7 @@ class Raqueta(Vigneta):
     def dibujar(self):
         pg.draw.rect(self.padre, self.color, (self.x, self.y, self.ancho, self.alto))
 
-    def mover(self):
+    def mover(self): #metodo porque son cosas que se hacen 
         teclas = pg.key.get_pressed()
         if teclas[pg.K_LEFT]:
             self.x -= self.vx
@@ -51,47 +60,47 @@ class Raqueta(Vigneta):
         if self.x + self.ancho >= self.padre.get_width():
             self.x = self.padre.get_width() - self.ancho
 
-class Bola:
-    def __init__(self, padre, x, y, color = (255, 255, 255), radio = 10, vx = 5, vy = 0):
-        self.x = x  #atributos(instancia dentro de una clase)
-        self.y = y 
-        self.color = color
+
+class Bola(Vigneta):
+    def __init__(self, padre, x, y, color = (255, 255, 255), radio = 10):
+        super().__init__(padre, x - radio, y - radio, 2 * radio, 2 * radio, color) #la coordenada de la bola es la esquina de la vigneta
         self.radio = radio 
         self.vx = 5
         self.vy = 5
-        self.padre = padre
-
 
     def mover(self):
         self.x += self.vx
         self.y += self.vy
 
-        if self.x <= self.radio or self.x >= self.padre.get_width() - self.radio: #si es positivo o negativo se controla desde aqui del sentido 
+        if self.x <= 0 or self.x >= self.padre.get_width() - self.ancho: #si es positivo o negativo se controla desde aqui del sentido 
             self.vx *= -1       
 
-        #if self.radio >= self.x >= limDer - self.radio (es lo mismo de arriba)
-
-        if self.y <= self.radio or self.y >= self.padre.get_height() - self.radio:
+        if self.y <= 0 or self.y >= self.padre.get_height() - self.alto:
             self.vy *= -1
 
     def dibujar(self): #superficie donde se va a dibujar 
-        pg.draw.circle(self.padre, self.color, (self.x, self.y), self.radio) #tupla 
+        pg.draw.circle(self.padre, self.color, (self.xcentro, self.ycentro), self.radio) #tupla 
 
     def compruebaChoque(self, otro):
-        if self.x - self.radio in range(otro.x, otro.x + otro.ancho) or \
-           self.x + self.radio in range(otro.x, otro.x + otro.ancho) and \
-           self.y - self.radio in range(otro.y, otro.y + otro.alto) or \
-           self.y + self.radio in range(otro.y, otro.y + otro.alto):
+        if (self.x in range(otro.x, otro.x + otro.ancho) or \
+           self.x + self.ancho in range(otro.x, otro.x + otro.ancho)) and \
+           (self.y in range(otro.y, otro.y + otro.alto) or \
+           self.y + self.alto in range(otro.y, otro.y + otro.alto)):
 
            self.vy *= -1
+
 
 
 class Game: 
     def __init__(self, ancho=600, alto=800): #generico los valor de ancho y alto 
         self.pantalla = pg.display.set_mode((ancho, alto))
-        self.bola = Bola(self.pantalla, ancho // 2, alto // 2)
+        self.bola = Bola(self.pantalla, ancho // 20, alto // 2, radio = 10)
         self.raqueta = Raqueta(self.pantalla, ancho//2, alto - 30, 100, 20) #esta intanciada
-        self.ladrillo = Ladrillo(self.pantalla, 10, 10, 100, 50)
+        self.ladrillos = []
+        
+        self.crea_ladrillos()
+
+        self.reloj = pg.time.Clock()
         """
         for i in range (random.randint(2,10)):
             radio = random.randint(5, 50)
@@ -106,13 +115,17 @@ class Game:
             self.bolas[i].vy = random.randint(5,15) * random.choice[(-1, 1)] 
         """
 
+    def crea_ladrillos(self):
+        for col in range(10):
+            for fil in range(4):
+                l = Ladrillo(self.pantalla, 5 + 60 * col, 5 + 30 * fil, 50, 20)
+                self.ladrillos.append(l)
 
     def bucle_ppal(self): #su trabajo es montarme la pantalla en infinito
         game_over = False
 
         while not game_over:
-            milisegundos = self.reloj.tick(60)
-            print(milisegundos)
+            milisegundos = self.reloj.tick(FPS)
 
             eventos = pg.event.get()
             for evento in eventos:
@@ -134,14 +147,14 @@ class Game:
 
             self.pantalla.fill((255, 0, 0)) #rellenar la pantalla.
             
-            self.ladrillo.mover()
             self.bola.mover()
             self.raqueta.mover()
             self.bola.compruebaChoque(self.raqueta)
+
             self.bola.dibujar()
             self.raqueta.dibujar()
-            self.ladrillo.dibujar()
-
+            for ladrillo in self.ladrillos:
+                ladrillo.dibujar()
             pg.display.flip()
 
 
